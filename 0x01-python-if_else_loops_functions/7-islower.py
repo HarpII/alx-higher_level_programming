e142ed86f49fd476f1d8dd7edcb6431cee375268
Harp <walesii19@gmail.com>
2022-11-29 21:03:53 +0000

    alternative method

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def islower(c):
-    for i in range(97, 123):
-        if chr(i) == c:
-            return True
-        else:
-            return False
+    return (ord('a') <= ord(c) <= ord('z'))
