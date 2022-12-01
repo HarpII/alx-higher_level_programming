ca7addaa786b81b8be7fc3c9ddcf2fb1aedf67c2
Harp <walesii19@gmail.com>
2022-12-01 19:49:28 +0000

    fixed PEP style

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,10 @@
 if __name__ == "__main__":
     from sys import argv
 
-    #total number of arguments
     argc = len(argv)
     sum = 0
 
     for i in range(1, argc):
-       sum += int(argv[i])
+        sum += int(argv[i])
 
     print("{}".format(sum))
