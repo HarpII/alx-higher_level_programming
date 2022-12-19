505cbc61887c21f3bd71ff3c7b25aec3997fc9fb
Harp <walesii19@gmail.com>
2022-12-19 12:47:32 +0000

    fixed PEP 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or roman_string is None:
         return (0)
-    
+
     integer = 0
-    roman_milestones = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_milestones = {"I": 1, "V": 5, "X": 10,
+                        "L": 50, "C": 100, "D": 500, "M": 1000}
 
     for i, j in zip(roman_string, roman_string[1:]):
         if i not in roman_milestones.keys():
