08818113452193445dae65c2b78a22d534317df2
Harp <walesii19@gmail.com>
2022-12-20 18:57:50 +0000

    initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    idx = 0
+
+    while idx < x:
+        try:
+            print("{}".format(my_list[idx]), end="")
+        except:
+            break
+
+        idx +=1
+    print("")
+    return idx
